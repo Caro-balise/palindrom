65058066e4200dd0c1d1ef0e0f2c13d4bbf8f861
Caro-balise <carolinebaer99@gmail.com>
2024-11-21 13:57:37 +0100

    simplification du code, il fonctionne maintenant

--- script.js
+++ script.js
@@ -27,39 +27,12 @@ const tableau = valeur.split('');
 console.log(tableau);
 
 
- function resultat(){
-   tableau.pop();
-tableau.shift();
-  console.log(tableau)
-  console.log(valeur + "est un palindrome")
-   affichage.innerText = valeur + " est un palindrome";
-   
+for (let i=0; i<(tableau.length); i++) {
+  
+  tableau[i]===tableau[tableau.length-i-1] ? (affichage.innerText = valeur + " est un palindrome") :  (affichage.innerText = valeur + " n'est pas un palindrome")
 }
-  
-  let retirerDernierElement = tableau.pop();
-  let retirerPremierElement = tableau.shift();
-  
-  
 
-if ( retirerDernierElement === retirerPremierElement && (tableau.length%2 == 0)) do {
-  
+}
 
-  resultat();
- 
-}while(tableau.length > 0 ) 
-  
-  
-  
-  else if 
-    
-    ( retirerDernierElement === retirerPremierElement && (tableau.length%2 !== 0)) do {
 
-resultat()
-}while(tableau.length > 1 ) 
-
-else
-  
-  {console.log(valeur + " n'est pas un palindrome")
-    affichage.innerText = valeur + " n' est pas un palindrome";
-  };})
- 
+)
